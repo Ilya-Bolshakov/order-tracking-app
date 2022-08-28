ab96f63a5a909d34a2c8d43c836b310b3dc14b4e
Илья Большаков <gamenest926@gmail.com>
2022-08-28 16:15:52 +0300

    order component has been added

---
+++ src/app/components/order/order.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IOrder } from 'src/app/models/IOrder';
+import { OrdersService } from 'src/app/services/orders.service';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+  id!: number;
+  private subscription: Subscription;
+
+  constructor(private activateRoute: ActivatedRoute, private service: OrdersService) { 
+    this.subscription = activateRoute.params.subscribe(params => this.id = params['id']);
+  }
+
+  order!: IOrder;
+
+  ngOnInit(): void {
+    this.order = this.service.getOrder(this.id); 
+  }
+
+}
