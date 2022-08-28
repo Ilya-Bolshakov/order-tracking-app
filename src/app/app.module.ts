0242b4bac76776baf8a7bb10faf5b03283418e1e
Илья Большаков <gamenest926@gmail.com>
2022-08-28 16:15:04 +0300

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { OrderListComponent } from './components/order-list/order-list.component';
+import { OrderComponent } from './components/order/order.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { NavigateComponentComponent } from './components/navigate-component/navigate-component.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrderListComponent
+    OrderListComponent,
+    OrderComponent,
+    MainMenuComponent,
+    NavigateComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
