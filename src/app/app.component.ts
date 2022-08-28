ad6f5c4881c047152266f05df35c931667a2ef1b
Илья Большаков <gamenest926@gmail.com>
2022-08-28 13:56:55 +0300

    app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IOrder } from './models/IOrder';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'order-tracking-app';
+  orders: IOrder[] = [];
+
+
+
+
+  ngOnInit(): void {
+    this.orders = orders-
+  }
+
+
 }
