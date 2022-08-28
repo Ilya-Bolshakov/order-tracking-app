fdc6c67e1fdd87266e38d01a5a4137d2f7176575
Илья Большаков <gamenest926@gmail.com>
2022-08-28 16:16:25 +0300

    added navigate-component

---
+++ src/app/components/navigate-component/navigate-component.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavigateComponentComponent } from './navigate-component.component';
+
+describe('NavigateComponentComponent', () => {
+  let component: NavigateComponentComponent;
+  let fixture: ComponentFixture<NavigateComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavigateComponentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavigateComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
