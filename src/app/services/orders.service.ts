f111c4842834baad7846138f1770b402ab55dfc9
Илья Большаков <gamenest926@gmail.com>
2022-08-28 16:17:08 +0300

    add OrdersService

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { IOrder } from '../models/IOrder';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,35 @@ import { Injectable } from '@angular/core';
 export class OrdersService {
 
   constructor() { }
+
+  orders: IOrder[] = [
+    {
+      id: 0,
+      firstName:'aa',
+      lastName: 'rnd1',
+      visitDate: new Date,
+      nameOrder: 'order1',
+      descritpion: 'desc'
+    },
+    {
+      id: 1,
+      firstName:'bb',
+      lastName: 'rnd2',
+      visitDate: new Date(2022, 5, 1),
+      nameOrder: 'order2',
+      descritpion: 'desc'
+    }
+  ];
+
+  getOrders(): IOrder[] {
+    
+    
+    return this.orders;
+  }
+
+  getOrder(id: number): IOrder {
+    let order: IOrder;
+    order = this.orders.find(e => e.id == id)!;
+    return order;
+  }
 }
