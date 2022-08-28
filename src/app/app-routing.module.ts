abf32f9dc922663483bb96dc80165dad11eeafd7
Илья Большаков <gamenest926@gmail.com>
2022-08-28 16:16:00 +0300

    fixing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { OrderListComponent } from './components/order-list/order-list.component';
+import { OrderComponent } from './components/order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main/:id', component: OrderComponent},
+  {path: 'main', component: MainMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
