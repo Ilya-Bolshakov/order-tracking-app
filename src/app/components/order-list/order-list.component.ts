b5d103ed3cdfd422830aa62808b68c7ba3f07851
Илья Большаков <gamenest926@gmail.com>
2022-08-31 23:11:09 +0300

    correct behavior order list

--- src/app/components/order-list/order-list.component.ts
+++ src/app/components/order-list/order-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IOrder } from 'src/app/models/IOrder';
+import { IPageModel } from 'src/app/models/IPageModel';
 import { OrdersService } from 'src/app/services/orders.service';
 
 
@@ -12,12 +13,21 @@ import { OrdersService } from 'src/app/services/orders.service';
 export class OrderListComponent implements OnInit {
 
   orders: IOrder[] = [];
+  pageModel!: IPageModel;
 
   constructor (private service: OrdersService) {
+    this.pageModel = {currentPage: 1, pageSize:5, totalLength: 0};
+    this.pageModel.currentPage = 1;
   }
-
+  onChanged(pageSize: number) {
+    this.pageModel.pageSize = pageSize;
+  }
   ngOnInit(): void {
-    this.orders = this.service.getOrders();
+    this.service.getOrders().subscribe(result => {
+      this.orders = result;
+      this.pageModel.totalLength = result.length;
+    });
+    this.pageModel.currentPage = 1;
+    this.pageModel.pageSize = 5;
   }
-
 }
