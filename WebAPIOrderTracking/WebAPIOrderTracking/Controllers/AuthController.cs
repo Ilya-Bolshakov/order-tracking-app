e5c068eaed345291bde29952e854d8f4a3526a83
Илья <gamenest926@gmail.com>
2022-09-07 22:58:12 +0300

    Refactoring

--- WebAPIOrderTracking/WebAPIOrderTracking/Controllers/AuthController.cs
+++ WebAPIOrderTracking/WebAPIOrderTracking/Controllers/AuthController.cs
@@ -36,17 +36,17 @@ namespace WebAPIOrderTracking.Controllers
                   {
                       if (SecretHasher.Verify(user.Password, findUser.Userpassword))
                       {
-                        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
-                        var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-                        var tokeOptions = new JwtSecurityToken(
-                        issuer: "https://localhost:7195",
-                        audience: "https://localhost:7195",
-                        claims: new List<Claim>(),
-                        expires: DateTime.Now.AddMinutes(5),
-                        signingCredentials: signinCredentials
-                        );
-                        var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
-                        return Ok(new AuthenticatedResponse { Token = tokenString });
+                          var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                          var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                          var tokeOptions = new JwtSecurityToken(
+                          issuer: "https://localhost:7195",
+                          audience: "https://localhost:7195",
+                          claims: new List<Claim>(),
+                          expires: DateTime.Now.AddMinutes(5),
+                          signingCredentials: signinCredentials
+                          );
+                          var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                          return Ok(new AuthenticatedResponse { Token = tokenString });
                       }
                   }
                   return Unauthorized();
