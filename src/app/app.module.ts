d38f24b4512c61b489c7f238ffe3e212cb49d519
Илья Большаков <gamenest926@gmail.com>
2022-09-24 02:02:00 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -64,7 +64,7 @@ export function tokenGetter() {
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
-        allowedDomains: ["https://ordertracking.somee.com"],
+        allowedDomains: ["ordertracking.somee.com"],
         disallowedRoutes: []
       }
     }),
